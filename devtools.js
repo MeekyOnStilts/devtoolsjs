c4ec611a2bfbf94baacf468c65f96c969f7985d9
MeekyOnStilts <98719576+MeekyOnStilts@users.noreply.github.com>
2024-10-27 21:54:14 -0500

    Update devtools.js

--- devtools.js
+++ devtools.js
@@ -1,2 +1,2 @@
-/// uDevTools.js
+/// eruda.js
 (function () {     var script =  document.createElement('script');    script.src="//cdn.jsdelivr.net/npm/eruda";     document.body.appendChild(script);    script.onload = function () {         eruda.init()     } })();
