5fb6577dc03090e00f74c00ad8c7e688a8008e72
MeekyOnStilts <98719576+MeekyOnStilts@users.noreply.github.com>
2024-10-27 20:31:54 -0500

    Update devtools.js

--- devtools.js
+++ devtools.js
@@ -1,11 +1,2 @@
 /// uDevTools.js
-window.addEventListener("keyup", event => {
-  if (event.ctrlKey && event.which === 192) {
-    let defaultCode = "(function () { var script = document.createElement('script') script.src='//cdn.jsdelivr.net/npm/eruda' document.body.appendChild(script) script.onload = function () { eruda.init() } })()"
-    let code = prompt("Eval:", defaultCode)
-    if (code.startsWith("javascript:")) {
-      code = code.substring(11)
-    }
-    eval(code)
-  }
-})
+window.addEventListener("keyup", e => { if (e.ctrlKey && e.which === 192) { let c = prompt("Eval:", "(function () { var script = document.createElement('script'); script.src='//cdn.jsdelivr.net/npm/eruda'; document.body.appendChild(script); script.onload = function () { eruda.init() } })()"); if (c.startsWith("javascript:")) c = c.substring(11); eval(c); } });
