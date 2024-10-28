f92c433fd778a4b4e3beec2a58110fc7ea7c44a6
MeekyOnStilts <98719576+MeekyOnStilts@users.noreply.github.com>
2024-10-27 20:14:16 -0500

    Update devtools.js

--- devtools.js
+++ devtools.js
@@ -1,18 +1,13 @@
 /// uDevTools.js
 window.addEventListener("keyup", event => {
-  // Check if Ctrl key is pressed and the key is `
-  if (event.ctrlKey && event.key === '`') {
-    // Check if Eruda is already loaded
-    if (typeof eruda === 'undefined') {
-      var script = document.createElement('script');
-      script.src = "//cdn.jsdelivr.net/npm/eruda";
-      document.body.appendChild(script);
-      script.onload = function () {
+  if (event.ctrlKey && event.which === 192) {
+    (function () {     
+      var script = document.createElement('script');    
+      script.src = "//cdn.jsdelivr.net/npm/eruda";     
+      document.body.appendChild(script);    
+      script.onload = function () {         
         eruda.init();
       };
-    } else {
-      // Toggle Eruda visibility
-      eruda.get('console').config.set('display', !eruda.get('console').config.get('display'));
-    }
+    })();
   }
 });
