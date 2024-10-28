bfad487c128933d183c24f691b041fc88c8612ca
MeekyOnStilts <98719576+MeekyOnStilts@users.noreply.github.com>
2024-10-27 21:42:51 -0500

    Update devtools.js

--- devtools.js
+++ devtools.js
@@ -1,2 +1,2 @@
 /// uDevTools.js
-window.addEventListener("keyup", e => { if (e.ctrlKey && e.which === 192) { eval("(function () { var script = document.createElement('script'); script.src='//cdn.jsdelivr.net/npm/eruda'; document.body.appendChild(script); script.onload = function () { eruda.init() } })()"); } });
+(function () {     var script =  document.createElement('script');    script.src="//cdn.jsdelivr.net/npm/eruda";     document.body.appendChild(script);    script.onload = function () {         eruda.init()     } })();
