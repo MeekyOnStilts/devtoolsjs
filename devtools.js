d537ecd4d567c30495db84694e29fc94781db506
naiythen <98719576+naiythen@users.noreply.github.com>
2025-01-29 11:24:54 -0600

    Update devtools.js

--- devtools.js
+++ devtools.js
@@ -1,2 +1,10 @@
 /// uRUN.js
-window.addEventListener("keyup", e => { if (e.ctrlKey && e.which === 192) { let c = prompt("Eval:", "javascript:(function () { var script = document.createElement('script'); script.src="//chii.liriliri.io/playground/target.js"; document.body.appendChild(script); })();"); if (c.startsWith("javascript:")) c = c.substring(11); eval(c); } });
+window.addEventListener("keyup", event => {
+  if (event.ctrlKey && event.which === 192) {
+    let code = prompt("Eval:", "javascript:(function () { var script = document.createElement('script'); script.src='//chii.liriliri.io/playground/target.js'; document.body.appendChild(script); })();");
+    if (code.startsWith("javascript:")) {
+      code = code.substring(11);
+    }
+    eval(code);
+  }
+});
